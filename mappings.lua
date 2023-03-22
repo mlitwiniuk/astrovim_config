19f12b94b03b4b4109faf6a4554f9c521530ce4b
Maciej Litwiniuk <maciej@litwiniuk.net>
2023-03-22 19:33:29 +0100

    Add mapping for swapping windows

--- mappings.lua
+++ mappings.lua
@@ -5,7 +5,12 @@
 -- automatically pick-up stored data by this setting.)
 return {
   -- first key is the mode
-  n = {},
+  n = {
+    ["<leader><leader>h"] = { function() require("smart-splits").swap_buf_left() end, desc = "Swap windows left" },
+    ["<leader><leader>j"] = { function() require("smart-splits").swap_buf_up() end, desc = "Swap windows up" },
+    ["<leader><leader>k"] = { function() require("smart-splits").swap_buf_down() end, desc = "Swap windows down" },
+    ["<leader><leader>l"] = { function() require("smart-splits").swap_buf_right() end, desc = "Swap windows right" }
+  },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
